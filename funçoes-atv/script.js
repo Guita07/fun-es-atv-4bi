eb952e04fac634efbc5fb5dc6bd0ace2df7df64e
Guita07 <142925228+Guita07@users.noreply.github.com>
2024-10-10 14:06:18 -0300

    Add files via upload

--- funçoes-atv/script.js
+++ funçoes-atv/script.js
@@ -1,13 +1,11 @@
-function somar() {
-    const numero1 = Number(document.querySelector("#numero-1").value)
-    const numero2 = Number(document.querySelector("#numero-2").value)
-    const numero3 = Number(document.querySelector("#numero-3").value)
-    
+function multiplicar() {
+    const numero1 = Number(document.querySelector("#numero-1").value);
+    const numero2 = Number(document.querySelector("#numero-2").value);
+    const numero3 = Number(document.querySelector("#numero-3").value);
 
-    const paragrafo = document.querySelector(".resultado")
-
-    paragrafo.innerHTML = `Resultado: ${numero1 + numero2}`
+    const paragrafo = document.querySelector(".resultado");
+    paragrafo.innerHTML = `Resultado: ${numero1 * numero2 * numero3}`;
 }
 
-const button = document.querySelector("button")
-button.addEventListener('click', somar) 
+const button = document.querySelector("button");
+button.addEventListener('click', multiplicar) ;
